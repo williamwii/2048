408b19a4971f00b943b2d0871ba38674329acae7
William You <williamwii3189@gmail.com>
2014-05-06 15:35:10 -0700

    bug fix in move

--- js/game_solver.js
+++ js/game_solver.js
@@ -74,146 +74,144 @@ GameSolver.prototype.cloneGrid = function (grid) {
 }
 
 GameSolver.prototype.move = function (grid, direction) {
-    for (var i=0; i<grid.length; i++) {
-        for (var j=0; j<grid[i].length; j++) {
-            if (grid[i][j] != null) {
-                grid[i][j].merged = false;
-            }
-        }
-    }
+    var newGrid = this.cloneGrid(grid);
 
     if (direction == UP) {
-        for (var i=0; i<grid.length; i++) {
-            var col = grid[i];
+        for (var i=0; i<newGrid.length; i++) {
+            var col = newGrid[i];
             for (var j=1; j<col.length; j++) {
                 if (col[j] != null) {
                     var tile = null;
                     for (var k=j-1; k>=0; k--) {
-                        if (grid[i][k] != null) {
-                            tile = grid[i][k];
+                        if (newGrid[i][k] != null) {
+                            tile = newGrid[i][k];
                             break;
                         }
                     }
                     if (tile != null) {
-                        if (!tile.merged && tile.value == grid[i][j].value) {
+                        if (!tile.merged && tile.value == newGrid[i][j].value) {
                             tile.value *= 2;
                             tile.merged = true;
-                            grid[i][j] = null;
+                            newGrid[i][j] = null;
                         }
                         else {
-                            grid[i][j].position.y = tile.position.y + 1;
-                            grid[i][tile.position.y+1] = grid[i][j];
-                            grid[i][j] = null;
+                            var tmp = newGrid[i][j];
+                            newGrid[i][j].position.y = tile.position.y + 1;
+                            newGrid[i][j] = null;
+                            newGrid[i][tile.position.y+1] = tmp;
                         }
                     }
                     else {
-                        grid[i][j].position.y = 0;
-                        grid[i][0] = grid[i][j];
-                        grid[i][j] = null;
+                        newGrid[i][j].position.y = 0;
+                        newGrid[i][0] = newGrid[i][j];
+                        newGrid[i][j] = null;
                     }
                 }
             }
         }
     }
     else if (direction == RIGHT) {
-        for (var i=grid.length-2; i>=0; i--) {
-            var col = grid[i];
+        for (var i=newGrid.length-2; i>=0; i--) {
+            var col = newGrid[i];
             for (var j=0; j<col.length; j++) {
                 if (col[j] != null) {
                     var tile = null;
-                    for (var k=i+1; k<grid.length; k++) {
-                        if (grid[k][j] != null) {
-                            tile = grid[k][j];
+                    for (var k=i+1; k<newGrid.length; k++) {
+                        if (newGrid[k][j] != null) {
+                            tile = newGrid[k][j];
                             break;
                         }
                     }
                     if (tile != null) {
-                        if (!tile.merged && tile.value == grid[i][j].value) {
+                        if (!tile.merged && tile.value == newGrid[i][j].value) {
                             tile.value *= 2;
                             tile.merged = true;
-                            grid[i][j] = null;
+                            newGrid[i][j] = null;
                         }
                         else {
-                            grid[i][j].position.x = tile.position.x - 1;
-                            grid[tile.position.x-1][j] = grid[i][j];
-                            grid[i][j] = null;
+                            var tmp = newGrid[i][j];
+                            newGrid[i][j].position.x = tile.position.x - 1;
+                            newGrid[i][j] = null;
+                            newGrid[tile.position.x-1][j] = tmp;
                         }
                     }
                     else {
-                        grid[i][j].position.x = grid.length - 1;
-                        grid[grid.length-1][j] = grid[i][j];
-                        grid[i][j] = null;
+                        newGrid[i][j].position.x = newGrid.length - 1;
+                        newGrid[newGrid.length-1][j] = newGrid[i][j];
+                        newGrid[i][j] = null;
                     }
                 }
             }
         }
     }
     else if (direction == DOWN) {
-        for (var i=0; i<grid.length; i++) {
-            var col = grid[i];
+        for (var i=0; i<newGrid.length; i++) {
+            var col = newGrid[i];
             for (var j=col.length-2; j>=0; j--) {
                 if (col[j] != null) {
                     var tile = null;
                     for (var k=j+1; k<col.length; k++) {
-                        if (grid[i][k] != null) {
-                            tile = grid[i][k];
+                        if (newGrid[i][k] != null) {
+                            tile = newGrid[i][k];
                             break;
                         }
                     }
                     if (tile != null) {
-                        if (!tile.merged && tile.value == grid[i][j].value) {
+                        if (!tile.merged && tile.value == newGrid[i][j].value) {
                             tile.value *= 2;
                             tile.merged = true;
-                            grid[i][j] = null;
+                            newGrid[i][j] = null;
                         }
                         else {
-                            grid[i][j].position.y = tile.position.y - 1;
-                            grid[i][tile.position.y-1] = grid[i][j];
-                            grid[i][j] = null;
+                            var tmp = newGrid[i][j];
+                            newGrid[i][j].position.y = tile.position.y - 1;
+                            newGrid[i][j] = null;
+                            newGrid[i][tile.position.y-1] = tmp;
                         }
                     }
                     else {
-                        grid[i][j].position.y = col.length - 1;
-                        grid[i][col.length - 1] = grid[i][j];
-                        grid[i][j] = null;
+                        newGrid[i][j].position.y = col.length - 1;
+                        newGrid[i][col.length - 1] = newGrid[i][j];
+                        newGrid[i][j] = null;
                     }
                 }
             }
         }
     }
     else if (direction == LEFT) {
-        for (var i=1; i<grid.length; i++) {
-            var col = grid[i];
+        for (var i=1; i<newGrid.length; i++) {
+            var col = newGrid[i];
             for (var j=0; j<col.length; j++) {
                 if (col[j] != null) {
                     var tile = null;
                     for (var k=i-1; k>=0; k--) {
-                        if (grid[k][j] != null) {
-                            tile = grid[k][j];
+                        if (newGrid[k][j] != null) {
+                            tile = newGrid[k][j];
                             break;
                         }
                     }
                     if (tile != null) {
-                        if (!tile.merged && tile.value == grid[i][j].value) {
+                        if (!tile.merged && tile.value == newGrid[i][j].value) {
                             tile.value *= 2;
                             tile.merged = true;
-                            grid[i][j] = null;
+                            newGrid[i][j] = null;
                         }
                         else {
-                            grid[i][j].position.x = tile.position.x + 1;
-                            grid[tile.position.x+1][j] = grid[i][j];
-                            grid[i][j] = null;
+                            var tmp = newGrid[i][j];
+                            newGrid[i][j].position.x = tile.position.x + 1;
+                            newGrid[i][j] = null;
+                            newGrid[tile.position.x+1][j] = tmp;
                         }
                     }
                     else {
-                        grid[i][j].position.x = 0;
-                        grid[0][j] = grid[i][j];
-                        grid[i][j] = null;
+                        newGrid[i][j].position.x = 0;
+                        newGrid[0][j] = newGrid[i][j];
+                        newGrid[i][j] = null;
                     }
                 }
             }
         }
     }
 
-    return grid;
+    return newGrid;
 }
